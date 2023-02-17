7c2623b16565bec67aa4cbb9f324515268911eb5
hoocdarealest <70364696+Hoocs151@users.noreply.github.com>
2023-02-17 22:07:01 +0700

    Update auto_accept.py

--- auto_accept.py
+++ auto_accept.py
@@ -1,6 +1,5 @@
 import pyautogui
 from python_imagesearch.imagesearch import imagesearch
-import time
 from colorama import init, Fore, Style
 
 # Initialize colorama
@@ -14,6 +13,7 @@ IMAGE_THRESHOLD = 0.8
 TIMELAPSE = 1
 
 pyautogui.FAILSAFE = False
+pyautogui.PAUSE = 0.5
 
 def click_image(img_path):
     pos = imagesearch(img_path, IMAGE_THRESHOLD)
@@ -23,15 +23,17 @@ def click_image(img_path):
     return True
 
 def wait_for_image(img_path):
-    while True:
-        if click_image(img_path):
-            break
-        time.sleep(TIMELAPSE)
+    pos = imagesearch(img_path, IMAGE_THRESHOLD)
+    while pos[0] == -1:
+        pos = imagesearch(img_path, IMAGE_THRESHOLD)
+    pyautogui.click(pos[0], pos[1])
 
 def is_game_cancelled():
-    accepted = imagesearch(acceptedButtonImg, IMAGE_THRESHOLD)
-    play = imagesearch(playButtonImg, IMAGE_THRESHOLD)
+    accepted, play = imagesearch(acceptedButtonImg, IMAGE_THRESHOLD), imagesearch(playButtonImg, IMAGE_THRESHOLD)
     return accepted[0] == -1 and not play[0] == -1
+
+def print_message(message, color):
+    print(f"{color}[ + ] {message} [ + ]{Style.RESET_ALL}")
 
 def run():
     while True:
@@ -39,7 +41,7 @@ def run():
 
         while True:
             if is_game_cancelled():
-                print(Fore.YELLOW + "[ + ] Game has been cancelled [ + ]" + Style.RESET_ALL)
+                print_message("Game has been cancelled", Fore.YELLOW)
                 break
             checkGameAvailableLoop()
 
@@ -48,10 +50,8 @@ def checkGameAvailableLoop():
         pos = imagesearch(acceptButtonImg, 0.8)
         if not pos[0] == -1:
             pyautogui.click(pos[0], pos[1])
-            print(Fore.GREEN + "[ + ] Game accepted [ + ]" + Style.RESET_ALL)
+            print_message("Game accepted", Fore.GREEN)
             break
-        
-        time.sleep(TIMELAPSE)
 
 if __name__ == '__main__':
     print(Fore.LIGHTCYAN_EX + """\ 
@@ -62,8 +62,8 @@ if __name__ == '__main__':
    █              ▀███▀            
   ▀                                
                                     """ + Style.RESET_ALL)
-    print(Fore.BLUE + "[ + ] Started Searching [ + ]" + Style.RESET_ALL)
+    print_message("Started Searching", Fore.BLUE)
     try:
         run()
     except Exception as e:
-        print(Fore.RED + "[ ! ] An error occurred: " + str(e) + Style.RESET_ALL)
+        print_message(f"An error occurred: {str(e)}", Fore.RED)
